178935162df93dd59fe8783b166bd66cc4e7808f
Kashiff Khan <kashiff.k@yahoo.com>
2020-10-09 18:42:18 -0400

    Imported and set up router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <header><h1>Hello</h1></header>
+    </div>
   );
 }
-export default App;
+export default App; 
