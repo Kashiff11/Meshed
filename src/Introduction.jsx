ef79c32e68e2679900808c2a85e1d13316a6ccf3
Kashiff Khan <kashiff.k@yahoo.com>
2020-10-15 14:23:31 -0400

    Mobile Styling Initialized

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Introduction = () => {
   return (
     <div className="introductionStyling">
-      <span>Welcome to Meshed Up Family. An uncomplicated tool to uncomplicate your life!</span>
+      <span>Welcome to Meshed Up Family. Uncomplicate your life.</span>
     </div>
   );
 };
